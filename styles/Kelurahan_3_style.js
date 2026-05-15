76db82779f86d1d3fcd8097170afad67e93ab48a
putriangellita89-spec <putriangellita89@gmail.com>
2026-05-16 01:52:23 +0800

    Update Kelurahan_3_style.js

--- styles/Kelurahan_3_style.js
+++ styles/Kelurahan_3_style.js
@@ -9,7 +9,7 @@ var style_Kelurahan_3 = function(feature, resolution){
     
     var labelText = ""; 
     var value = feature.get("");
-    var labelFont = "19.5px \'Open Sans\', sans-serif";
+    var labelFont = "19.5px 'Open Sans', sans-serif";
     var labelFill = "#323232";
     var bufferColor = "";
     var bufferWidth = 0;
@@ -17,14 +17,32 @@ var style_Kelurahan_3 = function(feature, resolution){
     var offsetX = 0;
     var offsetY = 0;
     var placement = 'point';
+
     if (feature.get("nm_kelurah") !== null) {
         labelText = String(feature.get("nm_kelurah"));
     }
+
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(133,182,111,1.0)'}),
-        text: createTextStyle(feature, resolution, labelText, labelFont,
-                              labelFill, placement, bufferColor,
-                              bufferWidth)
+        stroke: new ol.style.Stroke({
+            color: 'rgba(35,35,35,1.0)',
+            lineDash: null,
+            lineCap: 'butt',
+            lineJoin: 'miter',
+            width: 0.988
+        }),
+        fill: new ol.style.Fill({
+            color: 'rgba(133,182,111,0.0)'
+        }),
+        text: createTextStyle(
+            feature,
+            resolution,
+            labelText,
+            labelFont,
+            labelFill,
+            placement,
+            bufferColor,
+            bufferWidth
+        )
     })];
 
     return style;
